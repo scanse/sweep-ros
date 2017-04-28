932fd2e1a87eae9b6c6f9df9e1d8009bbd1fa472
Kent Williams <k3nt00@gmail.com>
2017-04-28 12:02:30 -0700

    Replace sweep sdk with updated version, cleaned up node code

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Scanse Sweep ROS Driver and Node
 
 This node is currently publishing a pointcloud2 msg. Use sweep2scan.launch for conversion to laserscan msg.
+
+## Compatibility:
+Currently, sweep-ros is only compatible with sweep firmware v1.1 or greater.
+
+You can check the firmware version installed on your sweep device by using a serial terminal (see [manual](https://s3.amazonaws.com/scanse/Sweep_user_manual.pdf)) or more easily using the sweep visualizer (see [instructions](https://support.scanse.io/hc/en-us/articles/224557908-Upgrading-Firmware)).
--- src/node.cpp
+++ src/node.cpp
@@ -1,7 +1,7 @@
 
 /*The MIT License (MIT)
  *
- * Copyright (c) 2016, Scanse LLC
+ * Copyright (c) 2017, Scanse, LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,84 +23,52 @@
  */
 
 #include "ros/ros.h"
-#include "sweep.h"
 #include <pcl/point_types.h>
 #include "sensor_msgs/PointCloud2.h"
 #include <pcl/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
-
-SweepDriver *drv = NULL;
-status result;
-
-bool checkDeviceInfo(SweepDriver *drv)
-{
-    status result;
-    sweep_response_info_device_t info_d;
-    sweep_response_info_version_t info_v;
-
-    if (drv->getDeviceInfo(&info_d))
-    {
-        ROS_ERROR_STREAM("Error, couldn't retrieve device info");
-    }
-
-    if (drv->getVersionInfo(&info_v))
-    {
-        ROS_ERROR_STREAM("Error, couldn't retrieve device info");
-    }
-
-    if (strncmp((const char *) info_v.model, "SWEEP", 5))
-    {
-        ROS_ERROR_STREAM("Error, unknown Sweep Model");
-        return false;
-    }
-
-    if (info_d.diagnostic != '0') //Check Device health is good
-    {
-        ROS_ERROR_STREAM("Error, device diagnostic info incorrect");
-        return false;
-    }
-
-    return true;
-}
+#include <sweep/sweep.hpp>
 
 void publish_scan(ros::Publisher *pub,
-                  sweep_response_scan_packet_t *nodes,
-                  size_t node_count, ros::Time start,
-                  double scan_time, std::string frame_id)
+                  const sweep::scan *scan, std::string frame_id)
 {
     pcl::PointCloud <pcl::PointXYZ> cloud;
     sensor_msgs::PointCloud2 cloud_msg;
 
-    float angle_f;
+    float angle;
+    int32_t range;
     float x;
     float y;
+    int i = 0;
 
     cloud.height = 1;
-    cloud.width = node_count;
+    cloud.width = scan->samples.size();
     cloud.points.resize(cloud.width * cloud.height);
 
-    for (size_t i = 0; i < node_count; i++)
+    for (const sweep::sample& sample : scan->samples)
     {
-        float range = (float) nodes[i].distance;
-        angle_f = INT_TO_FLOAT(nodes[i].angle);
+        range = sample.distance;
+        angle = ((float)sample.angle / 1000); //millidegrees to degrees
 
-        x = (range * cos(DEG2RAD(angle_f))) / 100;
-        y = (range * sin(DEG2RAD(angle_f))) / 100;
+        //Polar to Cartesian Conversion
+        x = (range * cos(DEG2RAD(angle))) / 100;
+        y = (range * sin(DEG2RAD(angle))) / 100;
 
         cloud.points[i].x = x;
         cloud.points[i].y = y;
+        i++;
     }
 
     //Convert pcl PC to ROS PC2
     pcl::toROSMsg(cloud, cloud_msg);
-    cloud_msg.header.frame_id = "laser_frame";
+    cloud_msg.header.frame_id = frame_id;
 
     ROS_DEBUG("Publishing a full scan");
     pub->publish(cloud_msg);
 }
 
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) try
 {
     //Initialize Node and handles
     ros::init(argc, argv, "sweep_node");
@@ -125,69 +93,30 @@ int main(int argc, char *argv[])
     ros::Publisher scan_pub = nh.advertise<sensor_msgs::PointCloud2>("pc2", 1000);
 
     //Create Sweep Driver Object
-    drv = new SweepDriver();
-
-    if (!drv)
-    {
-        fprintf(stderr, "Create Driver fail, exit\n");
-        return -2;
-    }
-
-    //Connect to Device
-    if (drv->connect(serial_port.c_str(), serial_baudrate, 1000))
-    {
-        fprintf(stderr, "Error, Serial Couldn't Connect\n");
-        delete drv;
-        return -1;
-    }
+    sweep::sweep device{serial_port.c_str()};
 
     //Send Rotation Speed
-    if (drv->changeMotorSpeed(rotation_speed))
-    {
-        ROS_ERROR_STREAM("Error, couldn't set rotation speed");
-    }
-    else
-    {
-        ROS_INFO("expected rotation frequency: %d (Hz)", rotation_speed);
-    }
+    device.set_motor_speed(rotation_speed);
 
-    //Verify Device Info
-    if (!checkDeviceInfo(drv))
-    {
-        ROS_ERROR_STREAM("Error, coudln't verify device info");
-    }
+    ROS_INFO("expected rotation frequency: %d (Hz)", rotation_speed);
 
     //Start Scan
-    drv->startScan();
+    device.start_scanning();
 
-    ros::Time start_scan_time;
-    ros::Time end_scan_time;
-    double scan_duration;
     while (ros::ok())
     {
-        sweep_response_scan_packet_t nodes[MAX_SCAN_PACKETS];
-        size_t count = _countof(nodes);
-
         //Grab Full Scan
-        start_scan_time = ros::Time::now();
-        result = drv->getCompleteScan(nodes, count, 3000);
-        end_scan_time = ros::Time::now();
+        const sweep::scan scan = device.get_scan();
 
-        scan_duration = (end_scan_time - start_scan_time).toSec();
+        publish_scan(&scan_pub, &scan, frame_id);
 
-        if (result == S_OK)
-        {
-            publish_scan(&scan_pub, nodes, count, start_scan_time, scan_duration, frame_id);
-        }
-        else if (result == S_FAIL)
-        {
-            ROS_ERROR_STREAM("Error, failed scan");
-            // The data is invalid, publish anyways
-            publish_scan(&scan_pub, nodes, count, start_scan_time, scan_duration, frame_id);
-        }
         ros::spinOnce();
     }
-    // destroy driver
-    delete drv;
-    return 0;
+
+    //Stop Scanning & Destroy Driver
+    device.stop_scanning();
+}
+
+    catch (const sweep::device_error& e) {
+      ROS_ERROR_STREAM("Error: " << e.what() << std::endl);
 }
